f36d5e5fd892b6a56bfbd7a0beec0adc13a1d283
Sagar Jadhav <sagarjadhav27@gmail.com>
2022-10-20 16:26:58 +0530

    Run Build

--- build/block-2/index.asset.php
+++ build/block-2/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '696095f60b7a6fc703e8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '24a40e6c9f268fe3d355');
